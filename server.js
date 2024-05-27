424f075493157d5339482813b2252877098cf838
Mirza-Anas <mirzanas194@gmail.com>
2024-05-27 18:57:51 +0530

    updating server url

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://social-connect-server.onrender.com/"],
   })
 );
 
